5008cb54968a0a680d0bbc766a84976ca571033d
spyder21er <spyder21er@gmail.com>
2021-07-19 06:10:59 +0800

    Register the bindings for UserService and UserServiceInterface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\UserService;
+use App\Services\UserServiceInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UserServiceInterface::class, UserService::class);
     }
 
     /**
