ca7a931880166cd1c5ccfe6271467fe3655c09f5
spyder21er <spyder21er@gmail.com>
2021-07-19 10:08:29 +0800

    Write a Feature test covering the event and event listener's functionalities

---
+++ tests/Feature/Events/UserSavedEventTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Feature\Events;
+
+use App\Events\UserSaved;
+use App\Listeners\SaveUserBackgroundInformation;
+use App\Models\Detail;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class SaveUserBackgroundInformationTest extends TestCase
+{
+    use DatabaseMigrations, RefreshDatabase, WithFaker;
+
+    /**
+     * @test
+     * @return void
+     */
+    public function test_user_saved_event_is_dispatch_when_creating_user()
+    {
+        Event::fake();
+
+        User::factory()->create();
+
+        Event::assertDispatched(UserSaved::class);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function test_user_saved_event_is_dispatch_when_updating_user()
+    {
+        Event::fake();
+
+        $user = User::factory()->create();
+        $user->update([
+            'email' => 'newemail@example.com',
+        ]);
+
+        Event::assertDispatched(UserSaved::class);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function test_save_user_backgound_information_is_handled_when_user_save_event_is_dispatched()
+    {
+        $previous_detail_count = Detail::all()->count();
+        User::factory()->create();
+
+        $this->assertTrue(Detail::all()->count() == $previous_detail_count + 4);
+    }
+}
